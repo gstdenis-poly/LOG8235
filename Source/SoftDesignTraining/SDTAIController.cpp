83777d6d4cd6abe3e1c49cb01bee44ea3bdf7e02
Felix Boudreau <felixboudreau16@gmail.com>
2022-09-30 20:25:26 -0400

    Comments and code cleaning

--- Source/SoftDesignTraining/SDTAIController.cpp
+++ Source/SoftDesignTraining/SDTAIController.cpp
@@ -26,12 +26,12 @@ void ASDTAIController::MoveForward()
     // We change the player rotation according to its current rotation and its direction.
     FRotator rotation = direction.ToOrientationRotator() - pawn->GetActorForwardVector().ToOrientationRotator();
     pawn->AddActorWorldRotation(rotation, false, (FHitResult*)nullptr, ETeleportType::None);
-
     pawn->AddMovementInput(direction, 1);
 }
 
 bool ASDTAIController::DetectWall() 
 {
+    //Raycast initialization
     FHitResult outHits;
     FCollisionObjectQueryParams objectQueryParams;
     objectQueryParams.AddObjectTypesToQuery(ECC_PhysicsBody);
@@ -40,30 +40,21 @@ bool ASDTAIController::DetectWall()
     FCollisionQueryParams queryParams = FCollisionQueryParams::DefaultQueryParam;
     queryParams.bReturnPhysicalMaterial = true;
 
+    //Compute the raycast vector
     APawn* pawn = GetPawn();
     FVector forward = pawn->GetActorForwardVector() * COLLISION_DETECTION_DISTANCE;
     FVector rayStart = pawn->GetActorLocation() * FVector(1, 1, 0) + FVector(0, 0, 254 /*Walls are at 254 on Z axis*/);
     FVector rayEnd = rayStart + forward;
     
     bool wallDetected = GetWorld()->LineTraceSingleByObjectType(outHits, rayStart, rayEnd, objectQueryParams, queryParams);
-    if (wallDetected) 
-    {
-        if(detectedObstacle == nullptr) detectedObstacle = outHits.GetComponent();
-        else if (detectedObstacle != outHits.GetComponent()) {
-            detectedObstacle = outHits.GetComponent();
-            rotationSide = rand() % 2 == 1 ? 1 : -1;
-        }
-        FVector newdirection = FVector::CrossProduct(FVector::UpVector, outHits.Normal);
-        direction = FVector(direction.X + (newdirection.X * rotationSide), direction.Y + (newdirection.Y * rotationSide), 0);
-        direction.Normalize();
-        GetCharacter()->GetCharacterMovement()->MaxWalkSpeed = GetCharacter()->GetCharacterMovement()->MaxWalkSpeed/3;
-        return true;
-    }
-    return false;
+
+    if (wallDetected) avoidObstacle(outHits);
+    return wallDetected;
 }
 
 bool ASDTAIController::DetectDeathFloor() 
 {
+    //Raycast initialization
     FHitResult outHits;
     FCollisionObjectQueryParams objectQueryParams;
     objectQueryParams.AddObjectTypesToQuery(ECC_PhysicsBody);
@@ -73,26 +64,18 @@ bool ASDTAIController::DetectDeathFloor()
     FCollisionQueryParams queryParams = FCollisionQueryParams::DefaultQueryParam;
     queryParams.bReturnPhysicalMaterial = true;
 
+    //Compute the raycast vector
     APawn* pawn = GetPawn();
     FVector forward = pawn->GetActorForwardVector() * COLLISION_DETECTION_DISTANCE;
     FVector rayStart = pawn->GetActorLocation() * FVector(1, 1, 0) + FVector(0, 0, 130 /*Death floors are at 130 on Z axis*/);
     FVector rayEnd = FVector(rayStart.X + forward.X, rayStart.Y + forward.Y, 130);
 
     bool deathFloorDetected = GetWorld()->LineTraceSingleByObjectType(outHits, rayStart, rayEnd, objectQueryParams, queryParams);
+    
     if (deathFloorDetected && outHits.GetComponent()->GetCollisionObjectType() == COLLISION_DEATH_OBJECT) 
-    {
-        if (detectedObstacle == nullptr) detectedObstacle = outHits.GetComponent();
-        else if (detectedObstacle != outHits.GetComponent()) {
-            detectedObstacle = outHits.GetComponent();
-            rotationSide = rand() % 2 == 1 ? 1 : -1;
-        }
-        FVector newdirection = FVector::CrossProduct(FVector::UpVector, outHits.Normal);
-        direction = FVector(direction.X + (newdirection.X * rotationSide), direction.Y + (newdirection.Y * rotationSide), 0);
-        direction.Normalize();
-        GetCharacter()->GetCharacterMovement()->MaxWalkSpeed = GetCharacter()->GetCharacterMovement()->MaxWalkSpeed / 3;
-        return true;
-    }
-    return false;
+        avoidObstacle(outHits);
+
+    return deathFloorDetected;
 }
 
 bool ASDTAIController::DetectPickup() 
@@ -130,6 +113,7 @@ bool ASDTAIController::DetectPickup()
 
 bool ASDTAIController::DetectPlayer(bool pickupDetected) 
 {
+    //Raycast initialization
     TArray<FHitResult> outHits;
     FCollisionObjectQueryParams objectQueryParams;
     objectQueryParams.AddObjectTypesToQuery(ECC_PhysicsBody);
@@ -140,6 +124,7 @@ bool ASDTAIController::DetectPlayer(bool pickupDetected)
     FCollisionQueryParams queryParams = FCollisionQueryParams::DefaultQueryParam;
     queryParams.bReturnPhysicalMaterial = true;
 
+    //Compute the raycast vector
     APawn* pawn = GetPawn();
     FVector forward = pawn->GetActorForwardVector() * COLLISION_DETECTION_DISTANCE;
     FVector rayStart = pawn->GetActorLocation() * FVector(1, 1, 0);
@@ -153,6 +138,7 @@ bool ASDTAIController::DetectPlayer(bool pickupDetected)
             if (outHit.GetComponent()->GetCollisionObjectType() != COLLISION_PLAYER)
                 continue;
 
+            //Sets the AI direction to either chase or run away from the player
             FVector playerDirection = outHit.GetActor()->GetActorLocation() - pawn->GetActorLocation();
             direction = pickupDetected ? -playerDirection : playerDirection;
             direction = FVector(direction.X, direction.Y, 0);
@@ -160,4 +146,22 @@ bool ASDTAIController::DetectPlayer(bool pickupDetected)
         }
     }
     return false;
+}
+
+void ASDTAIController::avoidObstacle(FHitResult outHits) 
+{
+    if (detectedObstacle == nullptr) detectedObstacle = outHits.GetComponent();
+    else if (detectedObstacle != outHits.GetComponent()) {
+        detectedObstacle = outHits.GetComponent();
+        rotationSide = rand() % 2 == 1 ? 1 : -1; //randomly select which side to rotate, to prevent the AI from getting stuck
+    }
+
+    //Sets direction to the median vector of the current direction and the obstacle's normal
+    //Allows to gradually redirect the AI to a new direction to avoid the obstacle
+    FVector newdirection = FVector::CrossProduct(FVector::UpVector, outHits.Normal);
+    direction = FVector(direction.X + (newdirection.X * rotationSide), direction.Y + (newdirection.Y * rotationSide), 0);
+    direction.Normalize();
+
+    //Slows the AI while approaching an obstacle
+    GetCharacter()->GetCharacterMovement()->MaxWalkSpeed = GetCharacter()->GetCharacterMovement()->MaxWalkSpeed / 3;
 }
--- Source/SoftDesignTraining/SDTAIController.h
+++ Source/SoftDesignTraining/SDTAIController.h
@@ -18,30 +18,40 @@ class SOFTDESIGNTRAINING_API ASDTAIController : public AAIController
 public:
     // main loop called every tick
     virtual void Tick(float deltaTime) override;
+
     /// <summary>
     /// move the character towards the current direction
     /// </summary>
     void MoveForward();
+
     /// <summary>
     /// Detect a possible collision with wall, change the character direction to avoid it.
     /// </summary>
     /// <returns>True if a wall is detected</returns>
     bool DetectWall();
+
     /// <summary>
     /// Detect a possible collision with death floor, change the character direction to avoid it.
     /// </summary>
     /// <returns>True if a death floor is detected</returns>
     bool DetectDeathFloor();
+
     /// <summary>
     /// Detect if the player is near, run from if powerer up, towards him if not.
     /// </summary>
     /// <returns>True if player is near</returns>
     bool DetectPlayer(bool pickupDetected);
+
     /// <summary>
     /// Detect if the pickup is near run towards it.
     /// </summary>
     /// <returns>True if pickup is near</returns>
     bool DetectPickup();
+
+    /// <summary>
+    /// Redirects the AI to avoid the obstacle detected (Wall or DeathFloor).
+    /// </summary>
+    void avoidObstacle(FHitResult outHits);
     
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, meta = (ClampMin = 1, ClampMax = 250))
         // Acceleration of the AI character, by default 1m.s^-2, can be set between 1 and 10
@@ -59,7 +69,7 @@ public:
         float speed = 0.f;
 
 private:
-    UPrimitiveComponent* detectedObstacle = nullptr;
+    UPrimitiveComponent* detectedObstacle = nullptr; //Keeps track of the current obstacle being rayCast.
     float rotationSide = 1;
     FVector direction = FVector(1, 0, 0);
 };
